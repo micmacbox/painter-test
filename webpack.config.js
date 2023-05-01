c987e637071b5e524d618c5d3ead3db4fea9836c
서주현 <beloved930223@gmail.com>
2023-05-02 00:35:22 +0900

    html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './example/index.ts',
+  entry: './public/index.ts',
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+  ],
   module: {
     rules: [
       {
